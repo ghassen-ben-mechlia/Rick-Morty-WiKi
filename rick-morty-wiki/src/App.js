cd94d7a2975ded908bad1127a3916e19faf36f50
= <=>
2021-12-07 15:06:11 +0100

    ready to work in it

---
+++ rick-morty-wiki/src/App.js
@@ -0,0 +1,11 @@
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap/dist/js/bootstrap'
+function App() {
+  return (
+    <div className="App">
+      <h1 className="text-center">Rick-Morty-WiKi</h1>
+    </div>
+  );
+}
+
+export default App;
